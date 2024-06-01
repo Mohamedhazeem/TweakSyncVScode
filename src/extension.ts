60865fdfd1da9997ef2e8919abcff44b65231f8e
Mohamed Hazeem <a.mohamedhazeem@gmail.com>
2024-06-01 09:45:47 +0530

    simple css file update

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 import * as vscode from "vscode";
 import { startServer, stopServer, sendMessageToChrome } from "./server/server";
 import { TempororyIdMode, TemporaryIds } from "./server/temporaryId";
+import { findAndReplaceCssSelectors, findCssFiles } from "./server/test";
 
 export function activate(context: vscode.ExtensionContext) {
   let server = vscode.commands.registerCommand("vscode.startserver", () => {
@@ -16,48 +17,76 @@ export function activate(context: vscode.ExtensionContext) {
   let Message = vscode.commands.registerCommand(
     "vscode.sendMessageToChrome",
     () => {
-		console.log("vscode.sendMessageToChrome");
+      console.log("vscode.sendMessageToChrome");
       sendMessageToChrome();
     }
   );
-  let injectTemporaryId = vscode.commands.registerCommand("vscode.injectTemporaryIds", () => {
-    console.log("Inject Temporary IDs command executed.");
-    const editor = vscode.window.activeTextEditor;
-    if (editor) {
+  let injectTemporaryId = vscode.commands.registerCommand(
+    "vscode.injectTemporaryIds",
+    () => {
+      console.log("Inject Temporary IDs command executed.");
+      const editor = vscode.window.activeTextEditor;
+      if (editor) {
         const document = editor.document;
-        if (document.languageId === 'javascript' || document.languageId === 'javascriptreact' || document.languageId === 'typescriptreact' || document.languageId === 'html') {
-            TemporaryIds(document, TempororyIdMode.inject);
-            console.log("Temporary IDs injected.");
+        if (
+          document.languageId === "javascript" ||
+          document.languageId === "javascriptreact" ||
+          document.languageId === "typescriptreact" ||
+          document.languageId === "html"
+        ) {
+          TemporaryIds(document, TempororyIdMode.inject);
+          console.log("Temporary IDs injected.");
         } else {
-            console.log("Document language is not supported for injecting temporary IDs.");
+          console.log(
+            "Document language is not supported for injecting temporary IDs."
+          );
         }
-    } else {
+      } else {
         console.log("No active text editor found.");
+      }
     }
-});
-let removeTemporaryId = vscode.commands.registerCommand("vscode.removeTemporaryIds", () => {
-  console.log("Inject Temporary IDs command executed.");
-  const editor = vscode.window.activeTextEditor;
-  if (editor) {
-      const document = editor.document;
-      if (document.languageId === 'javascript' || document.languageId === 'javascriptreact' || document.languageId === 'typescriptreact' || document.languageId === 'html') {
+  );
+  let removeTemporaryId = vscode.commands.registerCommand(
+    "vscode.removeTemporaryIds",
+    () => {
+      console.log("Inject Temporary IDs command executed.");
+      const editor = vscode.window.activeTextEditor;
+      if (editor) {
+        const document = editor.document;
+        if (
+          document.languageId === "javascript" ||
+          document.languageId === "javascriptreact" ||
+          document.languageId === "typescriptreact" ||
+          document.languageId === "html"
+        ) {
           TemporaryIds(document, TempororyIdMode.remove);
           console.log("Temporary IDs removed.");
+        } else {
+          console.log(
+            "Document language is not supported for remove temporary IDs."
+          );
+        }
       } else {
-          console.log("Document language is not supported for remove temporary IDs.");
+        console.log("No active text editor found.");
       }
-  } else {
-      console.log("No active text editor found.");
-  }
-});
-
+    }
+  );
+  const findcss = vscode.commands.registerCommand("vscode.findcss", () => {
+    findAndReplaceCssSelectors()
+      .then(() => {
+        console.log("CSS selectors updated successfully.");
+      })
+      .catch((err) => {
+        console.warn("Failed to find and replace CSS selectors", err);
+      });
+  });
 
   context.subscriptions.push(disposable);
   context.subscriptions.push(server);
   context.subscriptions.push(Message);
   context.subscriptions.push(injectTemporaryId);
   context.subscriptions.push(removeTemporaryId);
-
+  context.subscriptions.push(findcss);
 }
 // async function handleWebSocketMessage(message: any) {
 //   if (message.action === 'updateElement') {
