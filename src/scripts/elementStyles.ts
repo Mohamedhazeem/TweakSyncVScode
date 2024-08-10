8e2e1abf3d0fd68071b456f85081ba6fa99687e1
Mohamed Hazeem <a.mohamedhazeem@gmail.com>
2024-08-10 10:04:26 +0530

    Apply change on css exciting file

--- src/scripts/elementStyles.ts
+++ src/scripts/elementStyles.ts
@@ -6,25 +6,31 @@ export async function elementStyles(message: ElementStyles) {
   const { styles } = message;
   const { inline, external, temporaryId } = styles;
   const files = await vscode.workspace.findFiles("**/*.{css}");
-  let updatedCSS: string;
+
   for (const file of files) {
     console.log(`Processing file: ${file.toString()}`);
 
     try {
       const document = await vscode.workspace.openTextDocument(file);
-      let contentString = document.getText();
+      let originalContent = document.getText();
 
-      updatedCSS = await updateCSSContent(contentString, external);
-      console.warn(`${updatedCSS}`);
-      const fullRange = new vscode.Range(
-        document.positionAt(0),
-        document.positionAt(contentString.length)
-      );
-      await vscode.window.showTextDocument(document).then((editor) => {
-        editor.edit((editBuilder) => {
-          editBuilder.replace(fullRange, updatedCSS);
+      const updatedCSS = await updateCSSContent(originalContent, external);
+
+      // Check if the updated CSS is different from the current content
+      if (originalContent !== updatedCSS) {
+        console.warn(`Updating file: ${file.toString()}`);
+        const fullRange = new vscode.Range(
+          document.positionAt(0),
+          document.positionAt(originalContent.length)
+        );
+        await vscode.window.showTextDocument(document).then((editor) => {
+          editor.edit((editBuilder) => {
+            editBuilder.replace(fullRange, updatedCSS);
+          });
         });
-      });
+      } else {
+        console.log(`No changes needed for file: ${file.toString()}`);
+      }
     } catch (error) {
       console.error(`Error processing file ${file.toString()}: ${error}`);
     }
