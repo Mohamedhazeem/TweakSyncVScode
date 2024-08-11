33b17ca62116a7a32626d0919bda32bcf27bfd59
Mohamed Hazeem <a.mohamedhazeem@gmail.com>
2024-08-12 00:36:59 +0530

    webview cotent

---
+++ src/scripts/webView.ts
@@ -0,0 +1,26 @@
+import * as vscode from "vscode";
+import * as path from "path";
+
+export function getWebviewContent(panel: vscode.WebviewPanel, extensionPath: string) {
+  const scriptUri = panel.webview.asWebviewUri(
+    vscode.Uri.file(path.join(extensionPath, "out", "webview", "bundle.js"))
+  );
+
+  const styleUri = panel.webview.asWebviewUri(
+    vscode.Uri.file(path.join(extensionPath, "out", "webview", "styles", "index.css"))
+  );
+  return `
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <meta charset="UTF-8">
+            <title>React Webview</title>
+            <link href="${styleUri}" rel="stylesheet">
+        </head>
+        <body>
+            <div id="root"></div>
+            <script src="${scriptUri}"></script>
+        </body>
+        </html>
+    `;
+}
