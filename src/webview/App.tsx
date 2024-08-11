a07e91bc1e3580ce3bbf1e11bfdd21387b03bc61
Mohamed Hazeem <a.mohamedhazeem@gmail.com>
2024-08-12 01:27:17 +0530

    App and button

---
+++ src/webview/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "../components/ui/button";
+
+const App = () => {
+  return (
+    <>
+      <div className="check">Hello from React in a Webview!</div>
+      <Button className="initiateTweakSync">Start TweakSync for VS Code</Button>
+    </>
+  );
+};
+export default App;
--- src/webview/index.tsx
+++ src/webview/index.tsx
@@ -1,9 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import "./styles/index.css";
-const App = () => {
-  return <div className="check">Hello from React in a Webview!</div>;
-};
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(<App />);
