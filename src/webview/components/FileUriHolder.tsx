b5b0d529f0336257499dd75ae7b629a9c2236ec3
Mohamed Hazeem <a.mohamedhazeem@gmail.com>
2024-08-19 13:42:45 +0530

    empty list shown

--- src/webview/components/FileUriHolder.tsx
+++ src/webview/components/FileUriHolder.tsx
@@ -1,6 +1,7 @@
 import React, { memo } from "react";
 import FileUriEditRemove from "./FileUriEditAndRemove";
 import { FileIdMap } from "@/types/ElementTypes";
+import { SelectIcon } from "./icons/SelectIcon";
 
 type FileUriHolderType = {
   cssFiles: string[];
@@ -13,21 +14,44 @@ const FileUriHolder = memo(({ cssFiles, htmlReactFiles = [] }: FileUriHolderType
       <div className="html-react-container">
         <span className="htmlCssTitle">HTML Files</span>
         <div className="fileUriEditRemoveContainer">
-          {htmlReactFiles.map((fileIdMap, index) => (
-            <div key={fileIdMap.fileUri}>
-              <FileUriEditRemove file={fileIdMap.fileUri} index={index} isHtmlReact={true} />
+          {htmlReactFiles?.length > 0 ? (
+            htmlReactFiles.map((fileIdMap, index) => (
+              <div key={fileIdMap.fileUri}>
+                <FileUriEditRemove file={fileIdMap.fileUri} index={index} isHtmlReact={true} />
+              </div>
+            ))
+          ) : (
+            <div className="emptyList">
+              <p className="font-bold">Empty List</p>
+              <p className="instruction-subtext">
+                <p>Click the "</p>
+                <SelectIcon />
+                <p>Files" button to choose your HTML/React files.</p>
+              </p>
             </div>
-          ))}
+          )}
         </div>
       </div>
       <div className="css-container">
         <span className="htmlCssTitle">CSS Files</span>
         <div className="fileUriEditRemoveContainer">
-          {cssFiles.map((file, index) => (
-            <div key={file}>
-              <FileUriEditRemove file={file} index={index} />
+          {cssFiles.length > 0 ? (
+            cssFiles.map((file, index) => (
+              <div key={file}>
+                <FileUriEditRemove file={file} index={index} />
+              </div>
+            ))
+          ) : (
+            <div className="emptyList">
+              <p className="font-bold">Empty List</p>
+              <p className="text-red-600">You need at least one CSS file to apply styles.</p>
+              <p className="instruction-subtext">
+                <p>Click the "</p>
+                <SelectIcon />
+                <p>Files" button to choose your CSS files.</p>
+              </p>
             </div>
-          ))}
+          )}
         </div>
       </div>
     </div>
